d6b802823317ea05481ed428d94dff4acae86d19
weeezik <mkihanya.33@gmail.com>
2024-06-29 09:26:57 -0700

    Add array board code from notes
    Pseudocode has been added. Version 2 is defined by
    using an array for the board instead of string literals.

--- main.rb
+++ main.rb
@@ -2,4 +2,11 @@
 # __object__|__object__|__object__
 #   object  |  object  |  object
 
-puts "This is my Tic Tac Toe Game!"
+#use array
+
+#array.to_s
+#use array to make board
+array = ["_","_","|","_","_"]
+
+puts "This is my Tic Tac Toe Game!\n"
+puts array.to_s
