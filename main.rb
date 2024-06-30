49d66add127b2dc953e49f47a7f8838513a91ce6
weeezik <mkihanya.33@gmail.com>
2024-06-30 16:49:00 -0700

    Start of win condition check logic
    Logic of checking wins in each direction
    (diagonal, horizontal, and vertical) have yet to
    be done.

--- main.rb
+++ main.rb
@@ -35,10 +35,60 @@ all_players = [player_one, player_two]
 board_array = ['_', pos_one.position_state, '_', '|', '_', pos_two.position_state, '_', '|', '_', pos_three.position_state, '_', "\n",
                '_', pos_four.position_state, '_', '|', '_', pos_five.position_state, '_', '|', '_', pos_six.position_state, '_', "\n",
                ' ', pos_seven.position_state, ' ', '|', ' ', pos_eight.position_state, ' ', '|', ' ', pos_nine.position_state, ' ']
-
-puts "Tic Tac Toe! Player 1 is 'X'. Player 2 is 'O'.
-Use arrow keys to navigate. Press Enter to select position. Player 1 goes first. 
-PRESS ENTER TO BEGIN."
-puts board_array.join
 BLANK16 = '_'
 BLANK79 = ' '
+
+puts "Tic Tac Toe! Player 1 is 'X'. Player 2 is 'O'.
+Use any arrow key to navigate. Press Enter to select position. Player 1 goes first."
+puts board_array.join
+win_check_array = []
+
+def game_result_check(win_check_array)
+  if win_check_array.count > 5
+    puts "There are enough X's and O's to determine a winner."
+    # check three options (diagonal, horizontal, and verticle)
+    # declare winner/loser
+    # check 2: win_check_array.count = 9
+    # check three options diagonal, horizontal, and verticle)
+    # declare tie
+  else
+    puts "There are not enough X's and O's to determine a winner."
+  end
+end
+
+# Single player turn
+# until game_result_check(win_check_array) == true
+player_input = ' '
+current_position = 0
+while player_input != "\r"
+  player_input = STDIN.getch
+  next unless player_input == "\e"
+
+  case current_position
+  when (0..5)
+    current_position += 1
+    all_positions[current_position].position_state = all_players[0].player_side
+    all_positions[current_position - 1].position_state = BLANK16
+  when (6..7)
+    current_position += 1
+    all_positions[current_position].position_state = all_players[0].player_side
+    all_positions[current_position - 1].position_state = BLANK79
+  when current_position = 8
+    current_position = 0
+    all_positions[current_position].position_state = all_players[0].player_side
+    all_positions[8].position_state = BLANK79
+  end
+  puts "\n" + ['_', pos_one.position_state, '_', '|', '_', pos_two.position_state, '_', '|', '_', pos_three.position_state, '_', "\n",
+               '_', pos_four.position_state, '_', '|', '_', pos_five.position_state, '_', '|', '_', pos_six.position_state, '_', "\n",
+               ' ', pos_seven.position_state, ' ', '|', ' ', pos_eight.position_state, ' ', '|', ' ', pos_nine.position_state, ' '].join
+end
+
+# when enter key is pressed this runs
+puts "\n" + 'Next player turn.'
+win_check_array.push(all_positions[current_position])
+current_position = 0
+all_positions[current_position].position_state = all_players[1].player_side
+puts ['_', pos_one.position_state, '_', '|', '_', pos_two.position_state, '_', '|', '_', pos_three.position_state, '_', "\n",
+      '_', pos_four.position_state, '_', '|', '_', pos_five.position_state, '_', '|', '_', pos_six.position_state, '_', "\n",
+      ' ', pos_seven.position_state, ' ', '|', ' ', pos_eight.position_state, ' ', '|', ' ', pos_nine.position_state, ' '].join
+puts game_result_check(win_check_array)
